7e66e6b46118c6fb46441590cd44c2fb0982e012
mkujawinski <michal.kujawinski@gmail.com>
2016-08-03 23:44:29 +0200

    Simple MainWindow with red dot signals

--- PodcastStudio/MainWindow.xaml.cs
+++ PodcastStudio/MainWindow.xaml.cs
@@ -24,5 +24,21 @@ namespace PodcastStudio
         {
             InitializeComponent();
         }
+
+        private void MainWindow_OnKeyDown(object sender, KeyEventArgs e)
+        {
+            ChangeSingalStatus(Signal1, e, Key.D1, Key.NumPad1);
+            ChangeSingalStatus(Signal2, e, Key.D2, Key.NumPad2);
+            ChangeSingalStatus(Signal3, e, Key.D3, Key.NumPad3);
+            ChangeSingalStatus(Signal4, e, Key.D4, Key.NumPad4);
+        }
+
+        private void ChangeSingalStatus(Ellipse signal, KeyEventArgs e, Key requesteKey, Key alternativeKey)
+        {
+            if (e.Key == requesteKey || e.Key == alternativeKey)
+            {
+                signal.Visibility = signal.Visibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+            }
+        }
     }
 }
